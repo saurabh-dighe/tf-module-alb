b597925c9bcb385b5b2bf61fa678864c5af35199
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-14 14:49:00 +0530

    auto pushhh

--- alb.tf
+++ alb.tf
@@ -1,14 +1,14 @@
-#Provisions ALB
-resource "aws_lb" "alb" {
-  name               = "roboshop-${var.ENV}-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+# #Provisions ALB
+# resource "aws_lb" "alb" {
+#   name               = "roboshop-${var.ENV}-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.lb_sg.id]
+#   subnets            = [for subnet in aws_subnet.public : subnet.id]
 
-  enable_deletion_protection = false
+#   enable_deletion_protection = false
 
-  tags = {
-    Environment = "production"
-  }
-}
+#   tags = {
+#     Environment = "production"
+#   }
+# }
