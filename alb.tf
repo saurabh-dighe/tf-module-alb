830151193a731ca72aacc8f8abfea1bd1c4f0f97
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-14 15:36:34 +0530

    auto pushhh

--- alb.tf
+++ alb.tf
@@ -1,14 +1,14 @@
-# #Provisions ALB
-# resource "aws_lb" "alb" {
-#   name               = "roboshop-${var.ENV}-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.lb_sg.id]
-#   subnets            = [for subnet in aws_subnet.public : subnet.id]
+#Provisions ALB
+resource "aws_lb" "alb" {
+  name               = "roboshop-${var.ENV}-${var.ALB_NAME}"
+  internal           = var.INTERNAL
+  load_balancer_type = "application"
+  security_groups    = var.INTERNAL? [aws_security_group.allow_private.*.id] : [aws_security_group.allow_public.*.id]
+  subnets            = var.INTERNAL? [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID] : [data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID]
 
-#   enable_deletion_protection = false
+  enable_deletion_protection = false
 
-#   tags = {
-#     Environment = "ALB_NAME"
-#   }
-# }
+  tags = {
+    Environment = "${var.ALB_NAME}"
+  }
+}
