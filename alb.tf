5cd4c7cfdf54bc039428676a8a569abfa6d25043
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-14 15:44:42 +0530

    auto pushhh

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 #Provisions ALB
 resource "aws_lb" "alb" {
-  name               = "roboshop-${var.ENV}-${var.ALB_NAME}"
+  name               = "${var.ALB_NAME}-alb"
   internal           = var.INTERNAL
   load_balancer_type = "application"
   security_groups    = var.INTERNAL? aws_security_group.allow_private.*.id : aws_security_group.allow_public.*.id
