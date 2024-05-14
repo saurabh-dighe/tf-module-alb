1ed626e2270b95bd9f35eb8861a835481b58bbe1
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-14 15:39:30 +0530

    auto pushhh

--- locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  SSH_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["SSH_USERNAME"]
-  SSH_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["SSH_PASSWORD"]
-}
